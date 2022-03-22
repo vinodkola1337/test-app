d4b9f03951ab4c56492f1daf5dfdc720fac29715
vinodkola1337 <vinodkola1337@gmail.com>
2022-03-22 18:15:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode:'jit',
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./stories/**/*.{js,vue,ts}",
+    "./layouts/**/*.{js,vue,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
